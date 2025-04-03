c23636cacc547537e752611783e300918e6dba36
Tshepang <tshepangmog10@gmail.com>
2025-04-03 11:18:05 +0200

    Fixed the tabs color and added border

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,9 +20,9 @@ export default function Home() {
       </Container>
 
       <Center className={styles.tabsContainer}>
-        <Container>
-          <LocationTabs />
-        </Container>
+        {/* <Container> */}
+        <LocationTabs />
+        {/* </Container> */}
       </Center>
     </VStack>
   );
--- src/features/job-search/ui/job-location-tabs.tsx
+++ src/features/job-search/ui/job-location-tabs.tsx
@@ -2,25 +2,32 @@ import { TabsRoot, TabsList, TabsTrigger, TabsContent } from "@/components/ui/ta
 import { JobListingLinks } from "./job-listing-links";
 import styles from "./job-location-tabs.module.css";
 import { INDUSTRY_LINKS, LOCATION_LINKS } from "@/features/job-search/helpers/dummy-data";
+import { Container } from "@/components/ui";
 
 export function LocationTabs() {
   return (
     <TabsRoot defaultValue="location" className={styles.tabsContainer}>
-      <TabsList className={styles.tabsList}>
-        <TabsTrigger value="location" className={styles.tabsTrigger}>
-          Jobs by Location
-        </TabsTrigger>
-        <TabsTrigger value="industry" className={styles.tabsTrigger}>
-          Jobs by Industry
-        </TabsTrigger>
-      </TabsList>
+      <Container>
+        <TabsList className={styles.tabsList}>
+          <TabsTrigger value="location" className={styles.tabsTrigger}>
+            Jobs by Location
+          </TabsTrigger>
+          <TabsTrigger value="industry" className={styles.tabsTrigger}>
+            Jobs by Industry
+          </TabsTrigger>
+        </TabsList>
+      </Container>
 
       <TabsContent value="location" className={styles.tabsContent}>
-        <JobListingLinks links={LOCATION_LINKS} />
+        <Container>
+          <JobListingLinks links={LOCATION_LINKS} />
+        </Container>
       </TabsContent>
 
       <TabsContent value="industry" className={styles.tabsContent}>
-        <JobListingLinks links={INDUSTRY_LINKS} />
+        <Container>
+          <JobListingLinks links={INDUSTRY_LINKS} />
+        </Container>
       </TabsContent>
     </TabsRoot>
   );
