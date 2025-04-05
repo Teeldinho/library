e7b865552041d59c18d107b7481704c1093fb527
Tshepang <tshepangmog10@gmail.com>
2025-04-05 22:06:36 +0200

    Replaced auto-complete

--- src/features/job-search/ui/location-search.tsx
+++ src/features/job-search/ui/location-search.tsx
@@ -1,14 +1,13 @@
 "use client";
 
 import { useTranslations } from "next-intl";
-import { Autocomplete } from "@/components/ui/auto-complete/auto-complete";
 import { useStoreSearchParams } from "@/stores/nuqs/use-store-search-params";
 import styles from "@/features/job-search/ui/job-search-form.module.css";
 import { VStack, Label } from "@/components/ui";
 import { LocationRTO } from "@/features/job-search/models/mappers";
-import { Suspense } from "react";
 import { FetchResult } from "@/lib/api-helpers";
 import { hasMinChars, MIN_AUTOCOMPLETE_CHARS } from "@/lib/validators";
+import { AutoComplete } from "@/components/ui/auto-complete/auto-complete/auto-complete";
 
 type LocationSearchProps = {
   suggestionsPromise: Promise<FetchResult<LocationRTO[]>>;
@@ -19,20 +18,35 @@ export function LocationSearch({ suggestionsPromise }: LocationSearchProps) {
   const t = useTranslations("HomePage");
   const showHelpText = !hasMinChars(location);
 
+  const staticLocations: LocationRTO[] = [
+    { value: "london", label: "London, UK" },
+    { value: "new-york", label: "New York, USA" },
+  ];
+
   return (
     <VStack space="xs" className={styles.locationField}>
       <Label htmlFor="location" className={styles.label}>
         {t("locationLabel")}
       </Label>
-      <Suspense fallback={<div>Loading Location Input...</div>}>
-        <Autocomplete
-          suggestionsPromise={suggestionsPromise}
-          onInputChange={setLocation}
-          placeholder={t("locationPlaceholder")}
-          listId="location-datalist"
-          inputValue={location || ""}
-        />
-      </Suspense>
+
+      {/* <AutoComplete<LocationRTO>
+        suggestions={suggestionsPromise}
+        onSelect={(item) => setLocation(item.value)}
+        inputValue={location || ""}
+        onInputChange={(value) => setLocation(value)}
+        placeholder={t("locationPlaceholder")}
+        itemToString={(item) => item.label}
+      /> */}
+
+      <AutoComplete<LocationRTO>
+        suggestions={staticLocations}
+        onSelect={(item) => setLocation(item.value)}
+        inputValue={location || ""}
+        onInputChange={(value) => setLocation(value)}
+        placeholder={t("locationPlaceholder")}
+        itemToString={(item) => item.label}
+      />
+
       {showHelpText && (
         <Label variant="muted" size="sm">
           {t("locationInputHelp", { minChars: MIN_AUTOCOMPLETE_CHARS })}
